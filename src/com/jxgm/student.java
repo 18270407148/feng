04c5b9ce273669c446b7b193ad5e48287f197496
86182 <1055864046@qq.com>
2021-05-13 21:18:23 +0800

    上传了

--- src/com/jxgm/student.java
+++ src/com/jxgm/student.java
@@ -6,6 +6,55 @@ public class student {
     private Integer age;
     private String sex;
     private String address;
+    private String phone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
     public student() {
     }
