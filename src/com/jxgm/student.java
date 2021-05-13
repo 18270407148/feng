a8745cf5d7963054e28dd26b29e732e54a8502fa
86182 <1055864046@qq.com>
2021-05-13 20:40:38 +0800

    111

--- src/com/jxgm/student.java
+++ src/com/jxgm/student.java
@@ -5,6 +5,7 @@ public class student {
     private String name;
     private Integer age;
     private String sex;
+    private String address;
 
     public student() {
     }
@@ -14,5 +15,16 @@ public class student {
         this.name = name;
         this.age = age;
         this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                ", address='" + address + '\'' +
+                '}';
     }
 }
