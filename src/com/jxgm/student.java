7bf2d695f6d28561a71ca36dbbe0c8a90c599a4f
86182 <1055864046@qq.com>
2021-05-13 20:36:28 +0800

    123456562132

--- src/com/jxgm/student.java
+++ src/com/jxgm/student.java
@@ -3,4 +3,16 @@ package com.jxgm;
 public class student {
     private String id;
     private String name;
+    private Integer age;
+    private String sex;
+
+    public student() {
+    }
+
+    public student(String id, String name, Integer age, String sex) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
 }
